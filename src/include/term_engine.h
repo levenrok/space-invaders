3ae4970b75ccf16457922a0c7e8239b84c8a5595
levrotech <hi.levrotech@tutanota.com>
2024-12-09 22:13:02 +0530

    created window functions for creation and deletion

--- src/include/term_engine.h
+++ src/include/term_engine.h
@@ -1,4 +1,9 @@
 #ifndef __TERM_ENGINE_H__
 #define __TERM_ENGINE_H__
 
+#include <ncurses.h>
+
+void create_window(WINDOW*& window);
+void delete_window(WINDOW*& window);
+
 #endif  // __TERM_ENGINE_H__
--- src/term_engine.cpp
+++ src/term_engine.cpp
@@ -1,1 +1,12 @@
 #include "include/term_engine.h"
+
+void create_window(WINDOW*& window) {
+    window = initscr();
+    noecho();
+    cbreak();
+}
+
+void delete_window(WINDOW*& window) {
+    delwin(window);
+    endwin();
+}
