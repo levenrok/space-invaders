d1814344f6d4f2846f0185375a03881ceed835db
levrotech <hi.levrotech@tutanota.com>
2024-12-15 18:36:29 +0530

    drew the player's sprite at the correct location

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
+#include <string>
+#include <vector>
+
 #include "term_engine/include/term_engine.h"
 
 int main(void) {
     WINDOW* win_game;
+    std::vector<std::string> sprite_player = {
+        "   /\\",
+        "  |==|",
+        " /____\\",
+        " |.--.|",
+    };
 
     create_window(win_game);
+
+    draw_sprite(win_game, COLS / 2, LINES - sprite_player.size(), sprite_player);
 
     delete_window(win_game);
 }
