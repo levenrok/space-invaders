f32322cb2e03c86d64605caf7f111c9272698ab5
levrotech <hi.levrotech@tutanota.com>
2024-12-25 09:05:25 +0530

    setup the game window

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <raylib.h>
+#include <cstdlib>
+
+int main(void)
+{
+    int windowWidth = 750;
+    int windowHeight = 700;
+
+    InitWindow(windowWidth, windowHeight, "C++ Space Invaders");
+
+    CloseWindow();
+    exit(EXIT_SUCCESS);
+}
